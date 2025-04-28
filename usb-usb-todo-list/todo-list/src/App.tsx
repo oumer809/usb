de4288353db5205293b13774fd2c6d8870ff4870
oumer809 <oumersherif809@gmail.com>
2025-04-28 18:34:28 +0000

    arl commited

--- usb-usb-todo-list/todo-list/src/App.tsx
+++ usb-usb-todo-list/todo-list/src/App.tsx
@@ -4,7 +4,7 @@ import TodoList from "./TodoList"
 
 const App: React.FC = () => {
   return (
-    <div style={{ backgroundImage:"url('/src/assets/nature09.jpeg')"}} className=" bg-cover h-screen py-48 ">
+    <div >
       <TodoList />
     </div>
   )
